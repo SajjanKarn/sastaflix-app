e24cbb8ed39e18aed74f289b37b2f519fb40a50b
SajjanKarn <karnaa787@gmail.com>
2023-02-05 20:06:19 +0545

    made local setup

--- config/api.js
+++ config/api.js
@@ -1,1 +1,15 @@
-export const STREAMING_API_URL = 'https://sastaflix.onrender.com/stream';
+import * as Network from 'expo-network';
+
+export let STREAMING_API_URL = '';
+
+const getIpAddress = async () => {
+  try {
+    const ip = await Network.getIpAddressAsync();
+    STREAMING_API_URL = `http://${ip}:3000/stream`;
+    console.log(STREAMING_API_URL);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+getIpAddress();
